da5250224ae72c40431b08ba1a18ddb0382a3ee5
vikas-OP <67771093+vikas-OP@users.noreply.github.com>
2020-07-27 16:36:11 +0530

    Add files via upload

---
+++ week2/day1/open.js
@@ -0,0 +1,10 @@
+var request = new XMLHttpRequest();
+var url_string = "https://dog.ceo/api/breeds/list/all";
+// Open a new connection, using the GET request on the URL endpoint
+request.open("GET", url_string, true);
+request.send();
+request.onload = function () {
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response);
+  console.log(data);
+};
