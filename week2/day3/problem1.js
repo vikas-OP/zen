afe725404d55f458f73ec550b4ad36ffbf9e7dd1
vikas-OP <67771093+vikas-OP@users.noreply.github.com>
2020-07-29 15:04:52 +0530

    Add files via upload

---
+++ week2/day3/problem1.js
@@ -0,0 +1,8 @@
+//Write a function called “addFive”.
+//Given a number, “addFive” returns 5 added to that number.
+var num = 0;
+function addFive(num) {
+  return num + 5;
+}
+var result = addFive(num);
+console.log(result);
